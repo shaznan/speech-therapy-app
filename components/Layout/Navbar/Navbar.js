453965c41ccee5657f43c3775420aaf5f45fe31f
shaznan <shaznanfairozesally@gmail.com>
2021-05-19 00:57:41 +0530

    fix material ui grid issues, add app icon, make page responsive, add local state for show and hide navbar on <1280px , add css and transition effects

--- components/Layout/Navbar/Navbar.js
+++ components/Layout/Navbar/Navbar.js
@@ -1,32 +1,29 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import logoImage from "./images/logo.png";
 import classes from "./Navbar.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Grid from "@material-ui/core/Grid";
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Navbar() {
   const router = useRouter();
-  const menuItems = [
-    "Home",
-    "About-us",
-    "Articles",
-    "Book-store",
-    "Contact-us",
-  ];
+  const [isNavVisible, setIsNavVisible] = useState(false);
+  const menuItems = ["Home", "About us", "Articles", "Bookstore", "Contact us"];
 
-  const toLowerCase = (item) => {
-    return item[0].toLowerCase() + item.slice(1);
+  const formatItem = (item) => {
+    let toLowerCase = item[0].toLowerCase() + item.slice(1);
+    let removeSpacing = toLowerCase.replace(/\s+/g, "");
+    return removeSpacing;
   };
 
-  //   const styles = (theme) => ({
-  //     root: {
-  //       padding: theme.spacing.unit,
-  //       [`${theme.breakpoints.up("md")} and (orientation: portrait)`]: {
-  //         flexDirection: "row",
-  //       },
-  //     },
-  //   });
+  const onClickHandler = (e) => {
+    e.preventDefault();
+    setIsNavVisible(!isNavVisible);
+  };
+
+  // FIXME:
+  //find way to add two class names in jsx
 
   return (
     <Fragment>
@@ -34,28 +31,49 @@ function Navbar() {
         <Grid container spacing={0}>
           {" "}
           {/* Material Ui Grid layout */}
-          <Grid item xl={3} xs={12}>
-            <img className={classes.navlogo} src={logoImage} />
+          <Grid item lg={3} xs={12}>
+            <Link href="/">
+              <img className={classes.navlogo} src={logoImage} />
+            </Link>
           </Grid>
-          <Grid item xl={9} xs={12}>
-            <ul className={classes.navitems}>
-              <Grid container spacing={0}>
+          <Grid item lg={6} xs={12}>
+            <ul
+              className={`${classes.navitems} ${
+                isNavVisible ? classes.shownavitems : classes.hidenavitems
+              }`}
+            >
+              <Grid container className="classes.container" spacing={0}>
                 {/* create navbar items */}
-                {menuItems.map((item) => (
-                  <Grid item xs={1.5}>
+                {menuItems.map((item, i) => (
+                  <Grid key={i} item lg={2} xs={12}>
                     <li
                       className={
-                        router.pathname === "/" + toLowerCase(item) //convert in order to match exact route names
+                        router.pathname === "/" && item === "Home" //if empty pathname, highlight home navitem
+                          ? classes.active
+                          : router.pathname === "/" + formatItem(item) //convert in order to match exact route names
                           ? classes.active
                           : ""
                       }
                     >
-                      <Link href={"/" + toLowerCase(item)}>{item}</Link>
+                      <Link
+                        href={item == "Home" ? "/" : "/" + formatItem(item)}
+                      >
+                        {item}
+                      </Link>
                     </li>
                   </Grid>
                 ))}
               </Grid>
             </ul>
+          </Grid>
+          <Grid onClick={onClickHandler} item lg={3} xs={12}>
+            {/* <p className={classes.test}>hey</p> */}
+            <MenuIcon
+              fontSize="large"
+              className={`${classes.menuIcon} ${
+                isNavVisible ? classes.highlightmenuicon : ""
+              }`}
+            />
           </Grid>
         </Grid>
       </div>
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "../../components/Layout/Navbar/Navbar";
 
 function ArticlesPage() {
   return (
     <div>
+      <Navbar />
       <h1>Articles Page</h1>
     </div>
   );
