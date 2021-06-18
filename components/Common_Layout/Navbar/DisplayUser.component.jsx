4f05a229a97be0ef5d4e322831daf9c5802d2fab
shaznan <shaznanfairozesally@gmail.com>
2021-06-18 21:33:17 +0530

    make userdetails dropdown show and close on user click

--- components/Common_Layout/Navbar/DisplayUser.component.jsx
+++ components/Common_Layout/Navbar/DisplayUser.component.jsx
@@ -4,10 +4,12 @@ import classes from "./Navbar.module.css";
 import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import DisplayUserDropdown from "./DisplayUserDropdown.conponent";
 
 function DisplayUser() {
   const nickName = useSelector((state) => state.login_signup.nickName);
   const [avatar, setAvatar] = useState(null);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   //Get user Avatar
   useEffect(() => {
@@ -23,21 +25,26 @@ function DisplayUser() {
       .catch((err) => console.log(err));
   }, []);
 
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <Fragment>
       <Grid item lg={3} xs={4}>
-        <div className={classes.userdetailcontainer}>
-          {/* <div className={classes.avatarcontainer}>
+        <div onClick={toggleDropdown} className={classes.userdetailcontainer}>
+          <div className={classes.avatarcontainer}>
             <img src={avatar} className={classes.avatar} />
           </div>
           <span className={classes.userdetail}>
             Welcome, <span className={classes.placeholder}>{nickName}</span>{" "}
-          </span> */}
-          <div className={classes.dropdowncontainer}>
-            <img src={avatar} className={classes.dropdownavatar} />
-            <div className={classes.dropdownplaceholder}>{nickName}</div>
-            <div className={classes.logouttext}>Logout</div>
-          </div>
+          </span>
+          <DisplayUserDropdown
+            avatar={avatar}
+            nickName={nickName}
+            showDropdown={showDropdown}
+            toggleDropdown={toggleDropdown}
+          />
         </div>
       </Grid>
     </Fragment>
