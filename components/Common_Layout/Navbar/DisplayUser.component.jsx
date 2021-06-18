d4f5a20e20ff409fdd15c74eb4fa908b6a762a5c
shaznan <shaznanfairozesally@gmail.com>
2021-06-18 21:03:18 +0530

    create dropdown for userdetails, add logout option

--- components/Common_Layout/Navbar/DisplayUser.component.jsx
+++ components/Common_Layout/Navbar/DisplayUser.component.jsx
@@ -2,14 +2,42 @@ import React, { Fragment } from "react";
 import Grid from "@material-ui/core/Grid";
 import classes from "./Navbar.module.css";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function DisplayUser() {
   const nickName = useSelector((state) => state.login_signup.nickName);
+  const [avatar, setAvatar] = useState(null);
+
+  //Get user Avatar
+  useEffect(() => {
+    axios
+      .get(
+        `https://avatars.dicebear.com/api/initials/${"nigga"}.svg?backgroundColors=cyan`
+        // `https://avatars.dicebear.com/api/bottts/${nickName}.svg?textureChance=100`
+      )
+      .then((res) => {
+        console.log(res);
+        setAvatar(res.config.url);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <Fragment>
       <Grid item lg={3} xs={4}>
         <div className={classes.userdetailcontainer}>
-          <span className={classes.userdetail}>{nickName}</span>
+          {/* <div className={classes.avatarcontainer}>
+            <img src={avatar} className={classes.avatar} />
+          </div>
+          <span className={classes.userdetail}>
+            Welcome, <span className={classes.placeholder}>{nickName}</span>{" "}
+          </span> */}
+          <div className={classes.dropdowncontainer}>
+            <img src={avatar} className={classes.dropdownavatar} />
+            <div className={classes.dropdownplaceholder}>{nickName}</div>
+            <div className={classes.logouttext}>Logout</div>
+          </div>
         </div>
       </Grid>
     </Fragment>
