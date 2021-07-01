014fb21cd5ba285037dc7e81317b88acbe590ded
shaznan <shaznanfairozesally@gmail.com>
2021-07-01 16:56:17 +0530

    convert editor content to HTML by using draftHTML

--- components/Articles/ArticleForm/ArticleContnt.component.jsx
+++ components/Articles/ArticleForm/ArticleContnt.component.jsx
@@ -1,36 +1,73 @@
-// import { EditorState } from "draft-js";
+// // import { EditorState } from "draft-js";
 import dynamic from "next/dynamic";
 import { useStyles } from "./ArticleForm_styles";
+import { convertFromRaw, convertToRaw } from "draft-js";
+import { EditorState } from "draft-js";
+// import { stateToHTML } from "draft-js-export-html";
+import draftToHtml from "draftjs-to-html";
 
-//Render import only on client side
+import { useRef } from "react";
+
+// //Render import only on client side
 const Editor = dynamic(
   () => {
     return import("react-draft-wysiwyg").then((mod) => mod.Editor);
   },
   { ssr: false }
 );
+
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 import React, { useState } from "react";
 
 function ArticleContnt() {
   const classes = useStyles();
-  const [editorState, setEditorState] = useState(null);
-  const handleEditorStateChange = (contentState) => {
-    setEditorState(contentState);
+  const content = {
+    entityMap: {},
+    blocks: [
+      {
+        key: "637gr",
+        text: "Initialized from content state.",
+        type: "unstyled",
+        depth: 0,
+        inlineStyleRanges: [],
+        entityRanges: [],
+        data: {},
+      },
+    ],
+  };
+  const [contentState, setContentState] = useState(content);
+  //   const [rawState, setRawState] = useState(EditorState.createEmpty());
+
+  const onContentStateChange = (contentState) => {
+    console.log(contentState.blocks);
+    setContentState(contentState);
   };
 
-  //implement editor toolbar and content area
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(stateToHTML(contentState));
+    // setRawState(convertFromRaw(contentState));
+  };
+
   return (
     <div>
       <Editor
-        editorState={editorState}
+        // EditorState={contentState}
+        onContentStateChange={onContentStateChange}
+        // onChange={onChange}
         toolbarClassName={classes.toolbarClass}
         wrapperClassName={classes.wrapperClass}
         editorClassName={classes.editorClass}
         className={classes.editor_container}
-        onEditorStateChange={handleEditorStateChange}
+        // onEditorStateChange={onContentStateChange}
       />
+      <div
+        dangerouslySetInnerHTML={{ __html: draftToHtml(contentState) }}
+      ></div>
+      <button type="submit" onClick={handleSubmit}>
+        submit
+      </button>
     </div>
   );
 }
