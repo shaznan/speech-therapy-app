8403263961e328f9cedf3dd76d9a7e343cffb5f5
Shaznan <shaznanfairozesally@gmail.com>
2021-07-18 01:37:34 +0530

    show write aricle btn only if user is logged in

--- components/Articles/SelectTopic/WriteArticle.component.jsx
+++ components/Articles/SelectTopic/WriteArticle.component.jsx
@@ -3,7 +3,7 @@ import NoteAddOutlinedIcon from "@material-ui/icons/NoteAddOutlined";
 import { useStyles } from "../Articles_styles";
 import Button from "@material-ui/core/Button";
 import { articleSlice_Actions } from "../../../store/articlesSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function WriteArticle() {
   const classes = useStyles();
@@ -12,14 +12,18 @@ function WriteArticle() {
     dispatch(articleSlice_Actions.toggleShowArticleForm());
   };
 
+  const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
+
   return (
     <div>
-      <div className={classes.writearticle_cont}>
-        <Button className={classes.writearticle_btn} onClick={onHandleClick}>
-          <NoteAddOutlinedIcon className={classes.writearticle_icon} />
-          Write an article
-        </Button>
-      </div>
+      {isLoggedIn && (
+        <div className={classes.writearticle_cont}>
+          <Button className={classes.writearticle_btn} onClick={onHandleClick}>
+            <NoteAddOutlinedIcon className={classes.writearticle_icon} />
+            Write an article
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
