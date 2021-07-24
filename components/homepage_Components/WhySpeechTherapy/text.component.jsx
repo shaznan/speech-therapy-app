4192eb28793c2f6a4785795a4d5e50314dbf8776
Shaznan <shaznanfairozesally@gmail.com>
2021-07-24 13:34:09 +0530

    Update home page text

--- components/homepage_Components/WhySpeechTherapy/text.component.jsx
+++ components/homepage_Components/WhySpeechTherapy/text.component.jsx
@@ -14,8 +14,18 @@ function Text() {
               Why Speech Therapy?
             </Typography>
             <Typography className={classes.whyspeech_txt}>
-              We put your brain under stress to think harder in isolation. Its a
-              form of brain exercise unlike any other!
+              We put your brain under stress to think harder in isolation. The
+              tests that we currently conduct are tests to measure your semantic
+              fluency, tested by asking you to generate semantic category
+              exemplars eg: animals.
+            </Typography>
+            <Typography className={classes.whyspeech_txt}>
+              And phonemic fluency, assessed by asking the examinee to generate
+              words beginning with a single letter.
+            </Typography>
+            <Typography className={classes.whyspeech_txt}>
+              Their is no hard and fast rule to measure Brain Fog, trying out
+              our tests will indicate if it's time you go get yourself checked.
             </Typography>
           </div>
         </div>
