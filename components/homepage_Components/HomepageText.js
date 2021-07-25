508c20cf297d60a2f1b28bb16f6bff4e9b64f5ce
Shaznan <shaznanfairozesally@gmail.com>
2021-07-25 22:33:37 +0530

    fix bug regarding login

--- components/homepage_Components/HomepageText.js
+++ components/homepage_Components/HomepageText.js
@@ -11,7 +11,7 @@ function HomepageText() {
   return (
     <div>
       <Slide duration={700} bottom>
-        <h1 data-testid="heading">test</h1>
+        {/* <h1 data-testid="heading">test</h1> */}
         <Typography variant="h3" className={classes.mainheading}>
           Get Rid of BrainFog <br className={classes.linebreak} />
           And Elevate Your
