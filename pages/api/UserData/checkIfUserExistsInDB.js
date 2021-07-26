afa440eec62f451f5b9a40f009a437f2293680e0
Shaznan <shaznanfairozesally@gmail.com>
2021-07-26 17:24:05 +0530

    Update checkIfUserExistsInDB.js

--- pages/api/UserData/checkIfUserExistsInDB.js
+++ pages/api/UserData/checkIfUserExistsInDB.js
@@ -7,7 +7,7 @@ export default async (req, res) => {
 
     console.log(userId);
     const uri =
-      "mongodb+srv://shaznanfairoze:qmpGwieO89Yy1QM@speech-therapy-app.mb1pc.mongodb.net/UserData?retryWrites=true&w=majority";
+      "mongodb+srv://shaznanfairoze:qmpGwieO89Yy1QNM@speech-therapy-app.mb1pc.mongodb.net/UserData?retryWrites=true&w=majority";
     const client = new MongoClient(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -38,29 +38,5 @@ export default async (req, res) => {
       // }
       // perform actions on the collection object
     });
-
-    // const client = await MongoClient.connect(
-    //   "mongodb+srv://shaznanfairoze:qmpGwieO89Yy1QNM@speech-therapy-app.mb1pc.mongodb.net/UserData?retryWrites=true&w=majority",
-    //   { useUnifiedTopology: true },
-    // );
-    // const db = await client.db();
-    // const userCollection = db.collection("userCollection");
-    // const users = await userCollection.find({ localId: userId }).toArray();
-    // client.close();
-
-    // if (users.length > 0) {
-    //   res.status(200).json({ message: "user exists" });
-    // } else {
-    //   res.status(204).json({ message: "user doesn't exist" });
-    // }
   }
 };
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://<username>:<password>@speech-therapy-app.mb1pc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
