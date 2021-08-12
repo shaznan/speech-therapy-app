025aeabb9c998099d48cfaa8feec6b5e206d4944
Shaznan <shaznanfairozesally@gmail.com>
2021-08-13 02:31:03 +0530

    Add getstatic props for admin article  page

--- pages/admin/articles.js
+++ pages/admin/articles.js
@@ -5,14 +5,21 @@ import Navigation from "../../components/AdminDashboard/Navigation.component";
 import AdminPerformtes from "../../components/AdminDashboard/Admin_PerformTest/AdminPerformtest";
 import AdminArticles from "../../components/AdminDashboard/Admin_Articles/AdminArticles";
 import { useSelector } from "react-redux";
-import { useRouter } from "next/router";
 import { useDispatch } from "react-redux";
 import useVerifyUserAdmin from "../../components/AdminDashboard/useVerifyUserAdmin";
+import { articleSlice_Actions } from "../../store/articlesSlice";
+import { MongoClient } from "mongodb";
 
-function HomePage() {
+function HomePage(props) {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   const [isAdmin, verifyAdmin] = useVerifyUserAdmin();
   const userRole = useSelector((state) => state.user.entities[0].userRole);
+
+  useEffect(() => {
+    dispatch(articleSlice_Actions.setArticles(props.articles));
+  }, []);
 
   useEffect(() => {
     verifyAdmin();
@@ -30,6 +37,31 @@ function HomePage() {
       )}
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://shaznan:j77hFjvqaRPiva-@speech-therapy-app.mb1pc.mongodb.net/Article?retryWrites=true&w=majority",
+    { useUnifiedTopology: true, useNewUrlParser: true },
+  );
+  const db = client.db();
+  const ArticlesCollection = db.collection("Articles");
+  const Articles = await ArticlesCollection.find({}).toArray();
+  client.close();
+  return {
+    props: {
+      articles: Articles.map((article) => ({
+        article: article.article,
+        author: article.author,
+        authorEmail: article.authorEmail,
+        authorUrl: article.authorUrl,
+        isVerified: article.isVerified,
+        articleId: article.articleId,
+        _id: article._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
 }
 
 export default HomePage;
